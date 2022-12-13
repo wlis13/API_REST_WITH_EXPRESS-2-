7f5b7a847fbcc5f23da132ed127e594104d4158f
wlisses <wlissesfernando285@gmail.com>
2022-12-13 20:04:57 -0300

    feita função que trabalha com o método post para inserir novo valor no objeto.json

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Cruzeiro Esporte Clube',
+    initials: 'CEC',
+  },
+];
+
+app.get('/', (req, res) => {
+  res.status(200).json({ "text": "Hello World!" });
+});
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body }
+  teams.push(newTeam);
+  res.status(201).json({ team: newTeam })
+});
+
+app.get('/teams', (req, res) => {
+  res.status(200).json(teams)
+})
 
 module.exports = app;
