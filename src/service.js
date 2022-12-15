776623f83a086077e5b990a91195722587fe21b4
wlisses <wlissesfernando285@gmail.com>
2022-12-15 15:49:52 -0300

    criado endpoint para listar objeto-movie

--- src/service.js
+++ src/service.js
@@ -1,7 +1,25 @@
 const express = require('express');
 
+const fs = require('fs').promises;
+const path = require('path');
+
 const app = express();
 
 app.use(express.json());
+
+const readMovies = async () => {
+  const data = await fs.readFile(path.resolve(__dirname, '../movies.json'));
+  const jsonData = JSON.parse(data);
+  return jsonData;
+};
+
+app.use('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const getMovies = await readMovies();
+  const chooseForId = getMovies.filter((iten) => iten.id === Number(id));
+  res.status(200).send(chooseForId);
+});
+
+readMovies();
 
 module.exports = app;
